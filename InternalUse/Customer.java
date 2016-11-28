8efc76d5caf995d1cc32d30a2f57f9424718adf0
Colin Schultz <cjs176@pitt.edu>
2016-11-27 20:27:40 -0500

    keeping things in sync

--- InternalUse/Customer.java
+++ InternalUse/Customer.java
@@ -1,5 +1,7 @@
 import java.sql.*;
 
+import java.util.Scanner;
+
 public class Customer
 {
 	private static Connection connection;
@@ -11,7 +13,7 @@ public class Customer
 	public static void main(String[] args)
 	{
 		String username = "cjs176";
-		String password = "@";
+		String password = "";
 
 		String url = "jdbc:oracle:thin:@class3.cs.pitt.edu:1521:dbclass";
 
@@ -30,43 +32,52 @@ public class Customer
 			e.printStackTrace();
 		}
 		
-        try
-        {
-            // Form the query
-            statement = connection.createStatement();
-            String query = "SELECT * from Airline";
+        String input;
+        input = GetInput();
         
-            // Get the result
-            resultSet = statement.executeQuery(query);
-        
-            int counter = 1;
-        
-            // Print the results
-            while(resultSet.next())
-            {
-                System.out.println("Record " + counter + ": " + resultSet.getString(1)
-                                   + " " + resultSet.getString(2) + " " + resultSet.getString(3)
-                                   + " " + resultSet.getInt(4));
-                counter ++;
-            }
-        }
-        catch(SQLException Ex)
+        if(input.equals("1"))
         {
-            System.out.println("Error running the sample queries.  Machine Error: " +
-                               Ex.toString());
+            AddCustomer();
         }
-        finally
-        {
-            // CLose the statement
-            try
-            {
-                if (statement != null) statement.close();
-            }
-            catch (SQLException e)
-            {
-                System.out.println("Cannot close Statement. Machine error: "+e.toString());
-            }
-        }
+        
+//        // Sample Query
+//        try
+//        {
+//            // Form the query
+//            statement = connection.createStatement();
+//            String query = "SELECT * from Airline";
+//        
+//            // Get the result
+//            resultSet = statement.executeQuery(query);
+//        
+//            int counter = 1;
+//        
+//            // Print the results
+//            while(resultSet.next())
+//            {
+//                System.out.println("Record " + counter + ": " + resultSet.getString(1)
+//                                   + " " + resultSet.getString(2) + " " + resultSet.getString(3)
+//                                   + " " + resultSet.getInt(4));
+//                counter ++;
+//            }
+//        }
+//        catch(SQLException Ex)
+//        {
+//            System.out.println("Error running the sample queries.  Machine Error: " +
+//                               Ex.toString());
+//        }
+//        finally
+//        {
+//            // CLose the statement
+//            try
+//            {
+//                if (statement != null) statement.close();
+//            }
+//            catch (SQLException e)
+//            {
+//                System.out.println("Cannot close Statement. Machine error: "+e.toString());
+//            }
+//        }
         
         // Close the connection
         try
@@ -79,4 +90,22 @@ public class Customer
                                Ex.toString());
         }
 	}
+    
+    private static void AddCustomer()
+    {
+        System.out.println("Add customer");
+    }
+    
+    private static String GetInput()
+    {
+        String input;
+        
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Enter a number to select an action:");
+        System.out.println("1 - Add a customer");
+        input = in.nextLine();
+        
+        return input;
+    }
 }
